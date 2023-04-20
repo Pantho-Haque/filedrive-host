1df313d99097dc0032472aa8632c5f735f28d77a
Pantho <panthohaque927908@gmail.com>
2023-04-20 22:35:22 +0600

    seeding addded 1

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,4 +11,8 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate:fresh --force 
+
+echo "Seeding Database..."
+php artisan db:seed 
+
