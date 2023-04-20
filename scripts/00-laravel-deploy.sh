cd819decc9762e1dc62689d9dd9d173d6b3d1e81
Pantho <panthohaque927908@gmail.com>
2023-04-20 17:28:47 +0600

    debug 8

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,4 +11,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate --force --seed
+
+echo "Running server"
+php artisan serve
