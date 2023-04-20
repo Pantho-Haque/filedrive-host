9e253a12846370a86a8a561019fd3a67c5cb1a39
Pantho <panthohaque927908@gmail.com>
2023-04-20 22:22:17 +0600

    debug 12

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{path?}', function () {
     return view('welcome');
 })->where('path', '.*');
+
+Route::get('/storage/backendfiles/{id}/{folder_id}/{filename}', function ($id, $folder_id, $filename) {
+    $path = storage_path('app/public/backendfiles/' . $id . '/' . $folder_id. "/" . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
