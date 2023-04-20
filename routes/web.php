298258360f56960a9d618bd9951baedf4e072752
Pantho <panthohaque927908@gmail.com>
2023-04-20 23:27:19 +0600

    seeding addded 7

--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/storage/backendfiles/{id}/{folder_id}/{filename}', function ($id, $folder_id, $filename) {
-    $path = storage_path('app/public/backendfiles/' . $id . '/' . $folder_id. "/" . $filename);
-
-    if (!File::exists($path)) {
-        abort(404);
-    }
-
-    $file = File::get($path);
-    $type = File::mimeType($path);
-    $response = Response::make($file, 200);
-    $response->header("Content-Type", $type);
-
-    return $response;
-});
-
 
 Route::get('/{path?}', function () {
     return view('welcome');
