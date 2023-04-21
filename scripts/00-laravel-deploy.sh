fbcd241a5c4281015701c124e9f151fd8c87c379
Pantho <panthohaque927908@gmail.com>
2023-04-21 13:23:54 +0600

    deploy day2 1

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,7 +2,7 @@
 echo "Running composer"
 
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+# composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
 php artisan config:cache
@@ -10,13 +10,14 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate:fresh --force 
 
-echo "Seeding Database..."
-php artisan db:seed --force 
+# echo "Running migrations..."
+# php artisan migrate:fresh --force 
+
+# echo "Seeding Database..."
+# php artisan db:seed --force 
 
 
-echo "Link the storage..."
-php artisan storage:link --force 
+# echo "Link the storage..."
+# php artisan storage:link --force 
 
